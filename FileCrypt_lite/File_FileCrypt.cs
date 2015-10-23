fbe8db8b30d2abfc3ae551651d91f368d8cf6197
Andew Shabanov <Andew Shabanov>
2015-10-23 15:27:59 +0300

    DZ ALP
    Signed-off-by: Andew Shabanov <Andew Shabanov>

---
+++ FileCrypt_lite/File_FileCrypt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCrypt_lite
+{
+    class File_FileCrypt
+    {
+        String File_name;
+        String File_path;
+
+        public File_FileCrypt(string path)
+        {
+
+            //File_name;
+
+            //File_path;
+        }
+    }
+}
